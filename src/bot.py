796ba1c9dc154b1c5b0464ff417eef2ca56e1563
kylekrause3 <kkrause317@gmail.com>
2021-10-05 13:58:56 -0400

    "

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+
 import discord
 from dotenv import load_dotenv
 
@@ -7,6 +8,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
