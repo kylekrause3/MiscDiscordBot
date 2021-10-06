a17ba5c0bfc8573dbbfac240bcb2b206ca5e0ec3
kylekrause3 <kkrause317@gmail.com>
2021-10-06 14:00:08 -0400

    send message and it now responds

--- src/bot.py
+++ src/bot.py
@@ -1,17 +1,30 @@
 # bot.py
 import os
-
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+bot = discord.Client()
 
-client = discord.Client()
+prefix = "!"
 
 
-@client.event
+@bot.event # when it's in a server
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild_count = 0
 
-client.run(TOKEN)
+    for guild in bot.guilds:
+        print(f"- {guild.id} (name: {guild.name})")
+
+    guild_count = guild_count + 1
+
+    print("Miscellaneous Bot is in " + str(guild_count))
+
+
+@bot.event # when message is sent to a channel
+async def on_message(message):
+    if message.content == prefix + "What's up?":
+        await message.channel.send("Whaddup")
+    elif message.content == prefix + "prefix":
+        await message.channel.send(prefix) # TODO: !prefix x changes prefix to x
+
+
+bot.run("ODk0NjM4ODkwODY5MTk4ODk5.YVs7ZQ.K2rv7Uaw9gQO7RRFel7_nYB3p_k")
